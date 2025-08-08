68b18eaf686540e88607e56a0addf091baaa68ea
Usama Husnain <usamahusnain096@gmail.com>
2025-08-09 02:37:21 +0500

    Add the Sum and Subtraction Function in App Python File

---
+++ app.py
@@ -0,0 +1,9 @@
+# Create the Fucntion for SUM, Subtraction, Divition and Multiplication
+
+# Create The Sum Function
+def sum(a,b):
+    return a + b
+
+# Create the Subtraction Function
+def subtraction(a,b):
+    return a - b
