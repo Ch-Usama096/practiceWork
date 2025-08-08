f2b6bd5ac28f5beed6b36169af50f7d12ee9cb33
Usama Husnain <usamahusnain096@gmail.com>
2025-08-09 02:45:19 +0500

    Create the Function MUL & DIV

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ def sum(a,b):
 # Create the Subtraction Function
 def subtraction(a,b):
     return a - b
+
+# Create the Multiplication Function
+def multiplication(a,b):
+    return a * b
+
+# Create the Divition Function
+def divition(a , b):
+    return a / b
